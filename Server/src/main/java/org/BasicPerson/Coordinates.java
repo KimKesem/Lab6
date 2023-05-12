edf5fc3e37cf1ab168db26d9c8ffff55bb61614b
KimKesem <125126667+KimKesem@users.noreply.github.com>
2023-05-12 19:22:27 +0600

    Create Coordinates.java

---
+++ Server/src/main/java/org/BasicPerson/Coordinates.java
@@ -0,0 +1,13 @@
+package org.server.BasicPerson;
+
+public class Coordinates {
+
+    private Double x;
+    private float y;
+
+    public Coordinates(Double x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
