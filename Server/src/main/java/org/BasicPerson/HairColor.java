750f73831fb8857fa07c00fbe8b39bebdc6c3cce
KimKesem <125126667+KimKesem@users.noreply.github.com>
2023-05-12 19:22:52 +0600

    Add files via upload

---
+++ Server/src/main/java/org/BasicPerson/HairColor.java
@@ -0,0 +1,8 @@
+package org.server.BasicPerson;
+
+public enum HairColor {
+    GREEN,
+    BLACK,
+    BLUE,
+    WHITE;
+}
---
+++ Server/src/main/java/org/BasicPerson/Person.java
@@ -0,0 +1,76 @@
+package org.server.BasicPerson;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.server.ServerStuff.IDable;
+
+import java.util.Date;
+
+public class Person implements Comparable<Person>, IDable {
+    private int id;
+    private String name;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm a z")
+    private Date creationDate;
+    private Double height;
+    private float weight;
+    private String passportID;
+
+    private HairColor HairColor;
+    public Person(){
+
+    }
+
+
+    public Person(int id, String name, String passportID, Coordinates coordinates, Date creationDate,
+                  Double height, float weight, HairColor HairColor)  {
+        this.id = id;
+        this.name = name;
+        this.passportID=passportID;
+        this.creationDate = creationDate;
+        this.height = height;
+        this.weight = weight;
+        this.HairColor = HairColor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+    public float getWeight() {
+        return weight;
+    }
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+    public String getPassportID(){return passportID;}
+    public void setPassportID(String passportID){this.passportID=passportID;}
+
+    @Override
+    public String toString() {
+        return " Name: " + this.getName()+"; Passport: "+this.getPassportID() + "; Height: " + this.getHeight() + "; Weight: " + this.getWeight();
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        if (this.getHeight() > o.getHeight()) {
+            return 1;
+        } else if (this.getHeight() == o.getHeight()) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    @Override
+    public int detId() {
+        return id;
+    }
+}
